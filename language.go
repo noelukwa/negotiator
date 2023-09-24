2f9c7b60257422ba2fd7214d0aa38af0746acc7d
noelukwa <ukwauchechukwu@gmail.com>
2023-09-24 19:59:55 +0100

    feat: implement and clean up tests for accpeptLanguage

--- language.go
+++ language.go
@@ -1,1 +1,103 @@
 package negotiator
+
+import (
+	"errors"
+	"sort"
+	"strconv"
+	"strings"
+)
+
+type Lang struct {
+	Name    string
+	Quality float64
+}
+
+// ParseLanguages parses the Accept-Language header and returns a list of languages
+// accepted by the client, sorted by priority.
+func (n *Negotiator) ParseLanguages(available ...string) ([]string, error) {
+	acceptLanguage := n.req.Header.Get("Accept-Language")
+
+	if acceptLanguage == "" || len(available) == 0 || acceptLanguage == "*" {
+		return available, nil
+	}
+
+	parsedLanguages, err := splitLanguages(acceptLanguage)
+	if err != nil {
+		return nil, err
+	}
+
+	preferredLanguages := findPreferredLanguages(parsedLanguages, available)
+
+	sortLanguagesByPriority(preferredLanguages)
+
+	return getLanguages(preferredLanguages), nil
+}
+
+// splitLanguages splits the Accept-Language header into individual languages with quality values.
+// It returns an error if a quality value is invalid.
+// See https://tools.ietf.org/html/rfc7231#section-5.3.5 for details.
+func splitLanguages(acceptLanguage string) ([]Lang, error) {
+	languages := strings.Split(acceptLanguage, ",")
+	parsedLanguages := make([]Lang, 0, len(languages))
+
+	for _, languageStr := range languages {
+		language, err := parseLanguage(languageStr)
+		if err != nil {
+			return nil, err
+		}
+		if language.Quality > 0 {
+			parsedLanguages = append(parsedLanguages, *language)
+		}
+	}
+
+	return parsedLanguages, nil
+}
+
+// parseLanguage parses a language string into a Lang struct.
+// It returns an error if a quality value is invalid.
+func parseLanguage(languageStr string) (*Lang, error) {
+	language := strings.Split(languageStr, ";q=")
+	if len(language) == 1 {
+		return &Lang{Name: strings.TrimSpace(language[0]), Quality: 1}, nil
+	}
+
+	quality, err := strconv.ParseFloat(strings.TrimSpace(language[1]), 64)
+	if err != nil {
+		return nil, errors.New("failed to parse quality value")
+	}
+
+	return &Lang{Name: strings.TrimSpace(language[0]), Quality: quality}, nil
+}
+
+// findPreferredLanguages returns a list of languages that are available.
+func findPreferredLanguages(parsedLanguages []Lang, available []string) []Lang {
+	availableSet := make(map[string]struct{}, len(available))
+	for _, lang := range available {
+		availableSet[lang] = struct{}{}
+	}
+
+	preferredLanguages := make([]Lang, 0)
+	for _, lang := range parsedLanguages {
+		if _, ok := availableSet[lang.Name]; ok {
+			preferredLanguages = append(preferredLanguages, lang)
+		}
+	}
+
+	return preferredLanguages
+}
+
+// sortLanguagesByPriority sorts a list of languages by priority.
+func sortLanguagesByPriority(languages []Lang) {
+	sort.Slice(languages, func(i, j int) bool {
+		return languages[i].Quality > languages[j].Quality
+	})
+}
+
+func getLanguages(languages []Lang) []string {
+	result := make([]string, len(languages))
+	for i, language := range languages {
+		result[i] = language.Name
+	}
+
+	return result
+}
--- language_test.go
+++ language_test.go
@@ -1,1 +1,152 @@
-package negotiator
+package negotiator_test
+
+import (
+	"github.com/noelukwa/negotiator"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestNegotiator_ParseLanguage(t *testing.T) {
+
+	tests := []struct {
+		name           string
+		acceptLanguage string
+		expected       []string
+		available      []string
+	}{
+		{
+
+			"should return *",
+			"",
+			[]string{"*"},
+			nil,
+		},
+		{
+
+			"should return *",
+			"*",
+			[]string{"*"},
+			nil,
+		},
+		{
+
+			"should return *, en",
+			"*, en",
+			[]string{"*", "en"},
+			nil,
+		},
+		{
+
+			"should return *",
+			"*, en;q=0",
+			[]string{"*"},
+			nil,
+		},
+		{
+
+			"should return preferred languages",
+			"*;q=0.8, en, es",
+			[]string{"en", "es", "*"},
+			nil,
+		},
+		{
+
+			"should return preferred language",
+			"en",
+			[]string{"en"},
+			nil,
+		},
+		{
+			"should return empty list",
+			"en;q=0",
+			[]string{},
+			nil,
+		},
+		{
+
+			"should return preferred languages",
+			"en;q=0.8, es",
+			[]string{"es", "en"},
+			nil,
+		},
+		{
+			"should use highest preferred order on duplicate",
+			"en;q=0.9, es;q=0.8, en;q=0.7",
+			[]string{"en", "es"},
+			nil,
+		},
+		{
+
+			"should return en-US, en",
+			"en-US, en;q=0.8",
+			[]string{"en-US", "en"},
+			nil,
+		},
+		{
+
+			"should return es, en-US",
+			"en-US;q=0.8, es",
+			[]string{"es", "en-US"},
+			nil,
+		},
+		{
+
+			"should return en-US, en-GB",
+			"en-US, en-GB",
+			[]string{"en-US", "en-GB"},
+			nil,
+		},
+		{
+
+			"should return es, en-US",
+			"en-US;q=0.8, es",
+			[]string{"es", "en-US"},
+			nil,
+		},
+		{
+
+			"should return en-US, en-GB",
+			"en-US;foo=bar;q=1, en-GB;q=1",
+			[]string{"en-US", "en-GB"},
+			nil,
+		},
+		{
+		
+			"should use prefer fr over nl",
+			"nl;q=0.5, fr, de, en, it, es, pt, no, se, fi, ro",
+			[]string{"fr", "de", "en", "it", "es", "pt", "no", "se", "fi", "ro", "nl"},
+			nil,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			req := httptest.NewRequest(http.MethodGet, "/", nil)
+			req.Header.Set("Accept-Language", test.acceptLanguage)
+
+			neg := negotiator.New(req)
+			var actual []string
+
+			if test.available == nil {
+				actual, _ = neg.ParseLanguages()
+			} else {
+				actual, _ = neg.ParseLanguages(test.available...)
+			}
+
+			if len(test.expected) >= 1 {
+				for i, v := range actual {
+					if v != test.expected[i] {
+						t.Errorf("Expected %s Language , got %s", test.expected[i], v)
+					}
+					t.Logf("Expected %s Language , got %s", test.expected[i], v)
+				}
+			} else {
+				if len(actual) != len(test.expected) {
+					t.Errorf("Expected %s Languages , got %s", test.expected, actual)
+				}
+			}
+
+		})
+	}
+}
